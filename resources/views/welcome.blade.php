6be7a8a96609897ab848015d4958fd23883c0165
Dany59 <patignydany@gmail.com>
2021-10-19 09:57:09 +0200

    ajout police fin du header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,19 +11,37 @@
     <header>
         <nav>
         <div class="navLeft">
-        <img class="logo" src="\img\Frame.svg" alt="">
+        <img class="logo" src="\img\Frame.svg" alt="logo serv'drone">
 </div>
-        <div class="navRight">
+        <div class="navCenter">
             <ul>
                
                 <li><a>ACCUEIL</a></li>
                 <li><a href="">HISTOIRE</a></li>
                 <li><a href="">CONTACT</a></li>
                 <li><a href="">EVENEMENT</a></li>
+                
             </ul>
 </div>
+        <div class="panier">
+        <img src="\img\shopping-cart-solid.svg" alt="panier"> 
+        </div>
+            <div class="navRight">
+                <img src="\img\account_icon_138984.svg" alt="profil"> 
+            </div>
         </nav>
     </header>
+
+    <main>
+
+    <div class="imgAccueil"></div>
+
+    <div class="droneUn">
+        <h2 class="titreDrone">Serv'Drone class 1</h2>      
+    </div>
+    <p class="descriptionDroneUn">offre un package de 10 services avec une garantie de deux ans.</p>
+
+</main>
     
 </body>
 </html>
