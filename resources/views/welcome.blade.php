d80cb76a68e4018e715b601932add9d5cdca973f
Dany59 <patignydany@gmail.com>
2021-10-20 15:24:06 +0200

    début du footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,115 @@
 
 </div>
 
+<div class="droneDeux">
+    <div class="contientTitreDroneDeux">
+        <h2 class="titreDroneDeux">Serv'Drone class 2</h2>
+    </div>
+    <p class="descriptionDroneDeux">Offre un package de 15 services dont le ménage de votre domicile par drone avec une garantie de cinq ans.</p>
+
+    <div class="contientImage">
+  
+    <div class="pictosDeux">
+
+        <div class="divPictoDroneDeux">
+            <img class="pictoDroneDeux" src="/img/icon-iron.svg" alt="fer à repasser">
+            <img class="pictoDroneDeuxDeux" src="/img/icon-bucket.svg" alt="seau et produit ménager">
+        </div>
+
+        <div class="divPictoDroneDeuxBis">
+            <img class="pictoDroneDeuxBis" src="/img/icon-clean.svg" alt="chemise">
+            <img class="pictoDroneDeuxDeuxBis" src="/img/icon-bath.svg" alt="chat dans une baignoire">
+            <img class="pictoDroneDeuxDeuxBis" src="/img/icon-oil.svg" alt="huile essentiel">
+</div>
+
+    <div class="divButtonUn">
+    <button class="buttonDetail">En savoir plus</button>
+</div>
+
+
+    </div>
+</div>
+</div>
+
+
+
+<div class="droneTrois">
+        <div class="contientTitreDrone">
+        <h2 class="titreDrone">Serv'Drone class 3</h2>      
+</div>
+    <p class="descriptionDroneUn">Offre un package de 20 services dont la sécurisation de votre domicile par drone avec une garantie de cinq ans.</p>
+
+
+
+<div class="contientImage">
+  
+    <div class="pictos">
+
+        <div class="divPictoUn">
+            <img class="pictoUn" src="/img/icon-guard.svg" alt="">
+            <img class="pictoUn" src="/img/icon-call.svg" alt="">
+        </div>
+
+        <div class="divPictoDeux">
+            <img class="pictoDeux" src="/img/icon-cattleprod.svg" alt="">
+            <img class="pictoDeux" src="/img/icon-trap.svg" alt="">
+            <img class="pictoDeux" src="/img/icon-clue.svg" alt="">
+</div>
+
+    <div class="divButton">
+    <button class="buttonDetail">En savoir plus</button>
+</div>
+
+
+    </div>
+</div>
+
+
+
+</div>
+
 </main>
+
+<footer>
+    <div class="contientFooter">
+        <div class="reseaux">
+            <h2 class="titreReseaux">RESEAUX SOCIAUX</h2>
+            <div class="contientTwitter">
+            <div class="twitter">
+                <img src="/img/Twitter.png" alt="">
+            </div>
+                <div class="nomTwitter">
+                <p class="twt">@Serv'Drone</p>
+</div>
+</div>
+<div class="contientTwitter">
+            <div class="twitter">
+                <img src="/img/instagram.png" alt="">
+            </div>
+                <div class="nomTwitter">
+                <p class="twt">Serv'Drone_off</p>
+</div>
+</div>
+<div class="contientTwitter">
+            <div class="twitter">
+                <img src="/img/facebook.png" alt="">
+            </div>
+                <div class="nomTwitter">
+                <p class="twt">Serv'Drone</p>
+</div>
+</div>
+            </div>
+            <div class="titreNewsletter">
+            <h2 class="newletters">Newsletter</h2>
+            <div class="contientNewsletter">
+                <div class="champMail"></div>
+            <input class="mail" type="email" placeholder="entrez votre adresse mail">
+            <button type="submit" class="envoiAbsolute"> S'ABONNER</button>
+</div>
+</div>
+        </div>
+
+</footer>
     
 </body>
 </html>
