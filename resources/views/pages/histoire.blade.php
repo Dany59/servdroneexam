e1545196873777b63bea52eb93e441a2ebe20c55
Dany59 <patignydany@gmail.com>
2021-10-22 15:47:39 +0200

    page histoire fini

--- resources/views/pages/histoire.blade.php
+++ resources/views/pages/histoire.blade.php
@@ -34,15 +34,117 @@
             <h1 class="histoire">notre histoire</h1>
 
             <div class="equipe">
-                <div class="textEquipe">
+                <div class="divTextEquipe">
                     <p class="paraEquipe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
                     <p class="paraEquipe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
-                    <p class="paraEquipe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
+                    <p class="paraEquipe">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin.</p>
 
                 </div>
                 <div class="imgEquipe">
-                    <img src="/img/equipe.jpg" alt="">
+                    <img class="test" src="/img/equipe.jpg" alt="">
                 </div>
             </div>
+
+            <div class="div1916">
+                <div class="img1916">
+                    <img src="img/history-01.png" class="imgAvion1916" alt="">
+                </div>
+                <div class="text1916">
+                    <p class="para1916">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
+                </div>
+            </div>
+
+            <div class="div1930">
+                <div class="text1930">
+                    <p class="para1930">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
+                </div>
+                <div class="img1930">
+                    <img src="/img/history-02.png" class="imgAvion1930" alt="">
+                </div> 
+            </div>
+            <div class="div2000">
+                <div class="img2000">
+                    <img src="img/history-03.png" class="imgAvion2000" alt="">
+                </div>
+                <div class="text2000">
+                    <p class="para2000">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
+                </div>
+            </div>
+
+            <div class="div2021">
+                <div class="text2021">
+                    <p class="para2021">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at interdum metus. Cras laoreet lobortis nisl, sit amet placerat nunc auctor eu. Nam pretium est vel sollicitudin semper.</p>
+                </div>
+                <div class="img2021">
+                    <img src="/img/history-04.png" class="imgAvion2021" alt="">
+                </div> 
+            </div>
+
+            <footer>
+                <div class="contientFooter">
+               <div class="footerLeft">
+                   <div class="titreReseaux">
+                       <h2 class="reseauxSociaux">Réseaux Sociaux</h2>
+                   </div>
+                   <div class="listeReseau">
+                            <div class="reseau">
+                                <div class="imgReseau">
+                                <img src="/img/twitter.png" alt="">
+                                </div>
+                                <p class="nomReseau">@Serv'drone</p>
+                            </div>
+                            <div class="reseau">
+                                <div class="imgReseau">
+                                    <img src="/img/instagram.png" alt="">
+                                </div>
+                                <p class="nomReseau">Serv'drone_Off</p>
+                        </div>
+                        <div class="reseau">
+                            <div class="imgReseau">
+                                <img src="/img/facebook.png" alt="">
+                            </div>
+                            <p class="nomReseau">Serv'drone</p>
+                        </div>
+                    </div>
+                    </div>
+            
+                    <div class="footerMid">
+                        <div class="titreNews">
+                            <h2 class="newsletter">Newsletter</h2>
+                        </div>
+                        <form class="formNews" action="" method="POST">
+                        <input class="adressMail" type="mail" placeholder="Entrez votre adresse mail">
+                        <button class="btnNews">S'abonner</button>
+                        </form>
+                        <div class="cguCgv">
+                            <p class="espaceCgu">CGU</p>
+                            <p class="espaceCgu">|</p>
+                            <p class="espaceCgu">CGV</p>
+                            <p class="espaceCgu">|</p>
+                            <p class="espaceCgu">Politique de confidentialité</p>
+                        </div>
+                    </div>
+               
+                   <div class="footerRight">
+                    <div class="titreReseaux">
+                        <h2 class="partenaires">partenaires</h2>
+                    </div>
+                    <div class="listePartenaires">
+                             <div class="divPartenaire">
+                                 <p class="nomPartenaire">easy drone</p>
+                             </div>
+                             <div class="divPartenaire">
+                                 <p class="nomPartenaire">ct drone</p>
+                         </div>
+                         <div class="divPartenaire">
+                             <p class="nomPartenaire">drone moi la main</p>
+                         </div>
+                     </div>
+                     </div>
+                   </div>
+                   <p class="copyrights">SERV’DRONE - © 2021</p>
+            
+            
+            </footer>
 </body>
 </html>
