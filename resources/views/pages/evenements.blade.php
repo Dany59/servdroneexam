57e58f41df0a04c62c6d6a7d20f6d3a287c9f2d9
Dany59 <patignydany@gmail.com>
2021-11-06 15:08:01 +0100

    page evenement terminé

--- resources/views/pages/evenements.blade.php
+++ resources/views/pages/evenements.blade.php
@@ -32,6 +32,8 @@
         </nav>
     </header>
 
+    <main>
+
     <h1 class="titreEvenement">Evenements</h1>
 
     <div class="contientEvent">
@@ -62,19 +64,61 @@
 
     <h2 class="titrePresse">Presse</h2>
 
+ <div class="contientDeuxArticle">
+     <div class="premierArticle">
+         <div class="cadreRose">
 
-    <div class="contientDeuxPresse">
-        <div class="premierArticlePresse">
-            <div class="fondRose"><h2 class="titrepresse1">Le monde</h2>
-                <p class="descriptionPresse">la crise sanitaire dynamise la livraison par drone</p></div>
+
+         </div>
+         <h2 class="leMonde">le monde</h2>
+         <p class="descriptionArticle">la crise sanitaire dynamise la livraison par drone</p>
+
+     </div>
+
+     <div class="deuxiemeArticle">
+        <div class="cadreRose">
+
 
         </div>
+        <h2 class="leMonde">Format drone</h2>
+        <p class="descriptionArticle">adaptation du drone professionnel aux personnes en situation de handicap</p>
 
     </div>
 
+     </div>
+
+ </div>
+
+ <div class="contientDeuxArticle2">
+    <div class="troisiemeArticle">
+        <div class="cadreRose">
+
+
+        </div>
+        <h2 class="leMonde">futura tech</h2>
+        <p class="descriptionArticle">les drones au service de l'homme</p>
+
+    </div>
+
+    <div class="quatriemeArticle">
+       <div class="cadreRose">
+
+
+       </div>
+       <h2 class="leMonde">flying eye</h2>
+       <p class="descriptionArticle">réglementation européenne et nouvelles catégories de vol</p>
+
+   </div>
+
+    </div>
+
+</div>
+
+</main>
 
 
 
+    </main>
     <footer>
         <div class="contientFooter">
        <div class="footerLeft">
